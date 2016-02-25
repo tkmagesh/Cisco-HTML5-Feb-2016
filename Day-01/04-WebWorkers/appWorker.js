a506553d772dd8c88e3cbef9b77dd6ecaafd31f5
tkmagesh <tkmagesh77@gmail.com>
2016-02-25 09:50:45 +0530

    Web Worker with progress reporting

--- Day-01/04-WebWorkers/appWorker.js
+++ Day-01/04-WebWorkers/appWorker.js
@@ -1,15 +1,29 @@
 function doWork(){
 		//do something very important and complex here
-	for(var i=0; i<10000; i++)
+	for(var i=0; i<10000; i++){
 		for(var j=0; j<10000; j++)
 			for(var k=0; k<100; k++){
 				
 			}
+		if (i % 100 === 0){
+			var message = {
+				type : 'progress',
+				percentCompleted : (i/100)
+			};
+			self.postMessage(message);
+		}
+	}
+	var message = {
+				type : 'progress',
+		percentCompleted : 100
+	};
+	self.postMessage(message);
 }
 self.addEventListener("message", function(evtArg){
-	if (evtArg.data === 'start'){
+	var workerMessage = evtArg.data;
+	if (workerMessage.type === 'start'){
 		doWork();
-		self.postMessage('done');
+		self.postMessage({type : 'done'});
 	} else {
 		console.log("Worker received an unknown message - ", evtArg.data);
 	}
